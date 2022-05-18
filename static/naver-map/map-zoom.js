6a4f4d6eb5622732a6af81a16fc7ff1280198bfc
hyunmin <choihm9903@naver.com>
2022-05-18 19:10:45 +0900

    feat: naver-map

---
+++ static/naver-map/map-zoom.js
@@ -0,0 +1,7 @@
+var map = new naver.maps.Map('map', {
+        scaleControl: false,
+        logoControl: false,
+        mapDataControl: false,
+        zoomControl: true,
+        minZoom: 6
+    });
