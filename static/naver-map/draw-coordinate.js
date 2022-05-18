77d92bc7aba6acc62380cc6596e8764475afc2fd
hyunmin <choihm9903@naver.com>
2022-05-18 19:31:03 +0900

    feat: map-coordinate

--- static/naver-map/draw-coordinate.js
+++ static/naver-map/draw-coordinate.js
@@ -1,25 +1,21 @@
-var HOME_PATH = window.HOME_PATH || '.';
-
-var cityhall = new naver.maps.LatLng(37.5666805, 126.9784147),
-    map = new naver.maps.Map('map', {
-        center: cityhall.destinationPoint(0, 500),
-
+var map = new naver.maps.Map('map', {
         scaleControl: false,
         logoControl: false,
         mapDataControl: false,
         zoomControl: true,
         minZoom: 6
-    }),
-    marker = new naver.maps.Marker({
-        map: map,
-        position: cityhall
     });
+
+var marker = marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.5666805, 126.9784147),
+    map: map,
+});
 
 var contentString = [
         '<div class="iw_inner">',
         '   <h3>서울특별시청</h3>',
         '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />',
-        '       <img src="'+ HOME_PATH +'/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
+        '       <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=f180_180&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_196%2F1440995637788dnnse_JPEG%2F11556036_0.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
         '       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />',
         '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
         '   </p>',
@@ -37,5 +33,3 @@ naver.maps.Event.addListener(marker, "click", function(e) {
         infowindow.open(map, marker);
     }
 });
-
-infowindow.open(map, marker);
