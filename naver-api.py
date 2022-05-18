a885d454be7f92446f85836ac39848adb3ecad74
hyunmin <choihm9903@naver.com>
2022-05-18 12:12:15 +0900

    feat: project

--- naver-api.py
+++ naver-api.py
@@ -29,5 +29,23 @@ def search(query):
         print("Error Code:" + rescode)
 
 
+
+def image(query):
+    client_id = "IDEZlxRWK9_vy7ZmJbtf"
+    client_secret = "ofIrQU9z72"
+    encText = urllib.parse.quote(query)
+    url = f"https://openapi.naver.com/v1/search/image?query={encText}&display=1"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if (rescode == 200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
+
 hot_place('서울')
 search('롯데월드')
+image('롯데월드')
