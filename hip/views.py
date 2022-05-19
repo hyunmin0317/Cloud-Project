42269fed4557f2da2203517189483f3d65a061ca
hyunmin <choihm9903@naver.com>
2022-05-19 09:56:58 +0900

    feat: seoul-hotplace

--- hip/views.py
+++ hip/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from hip.naver_api import hot_place
 
 def home(request):
-    context = {"data_list":hot_place("서울")}
+    data_list = []
+    regions = ["강남구", "강동구", "강북구", "강서구", "관악구", "광진구",
+              "구로구", "금천구", "노원구", "도봉구", "동대문구", "동작구",
+              "마포구", "서대문구", "서초구", "성동구", "성북구", "송파구",
+              "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"]
+    for region in regions:
+        data_list += hot_place(region)
+    context = {"data_list":data_list}
     return render(request, 'home.html', context)
